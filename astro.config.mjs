a831397b44642d0c5bfa534a0fab45ce036cdd75
Shoirhi <dibella.flqxl@simplelogin.com>
2023-12-22 13:38:16 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? "https://bolstatech.com" : "http://localhost:4321",
+  site: process.env.CI ? "https://bolstatech-astro.pages.dev" : "http://localhost:4321",
   integrations: [tailwind()],
 });
