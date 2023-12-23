38a0aa0b1c66db8affb462975de933d77125cb73
Shoirhi <dibella.flqxl@simplelogin.com>
2023-12-23 14:53:42 +0900

    サイトマップを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? "https://bolstatech.com" : "http://localhost:4321",
   redirects: {
-    "/blog": "/blog/p/1",
+    "/blog": "/blog/p/1"
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()]
 });
